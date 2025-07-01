a55fdde7d8e5a17dd88b16451adbef5b2907eeb3
Danna Mendez <danna.mendezmoncada@gmail.com>
2025-07-01 14:46:59 -0500

    intentando subir a ec2

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()  ],
-  // server: {
-  //   port: 3001, // ← cambia el puerto aquí
-  // },
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true,        // ← Esto permite conexiones externas (0.0.0.0)
+    port: 5173
+  }
 })
