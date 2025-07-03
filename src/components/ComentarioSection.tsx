5574c2ad958585ea9640e4ff3ae27278b119ce97
Danna Mendez <danna.mendezmoncada@gmail.com>
2025-07-02 20:02:02 -0500

    subiendo

--- src/components/ComentarioSection.tsx
+++ src/components/ComentarioSection.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-const API_URL = import.meta.env.API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 interface Comentario {
   id: number;
